c09530358f1a910b4beba8f25aecb8af6bfb0dc1
Joe Lee <lefodev@gmail.com>
2022-09-21 17:03:00 -0700

    update

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ import * as dat from "lil-gui";
 // debugging
 const gui = new dat.GUI();
 const parameters = {
-    materialColor: '#ffeded'
+    materialColor: '#634f72'
 };
 gui
     .addColor(parameters, 'materialColor')
     .onChange(() => {
         material.color.set(parameters.materialColor);
+        particlesMaterial.color.set(parameters.materialColor);
 });
 
 // boilerplate
@@ -22,10 +23,10 @@ const scene = new THREE.Scene();
 
 // objects
 const material = new THREE.MeshToonMaterial({color: parameters.materialColor});
-const objectsDistance = 4;
+const objectsDistance = 6;
 
 const mesh1 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
     material
 );
 const mesh2 = new THREE.Mesh(
@@ -40,8 +41,38 @@ const mesh3 = new THREE.Mesh(
 mesh1.position.y = objectsDistance * 0;
 mesh2.position.y = objectsDistance * -1;
 mesh3.position.y = objectsDistance * -2;
+
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
+
 const sectionMeshes = [ mesh1, mesh2, mesh3 ];
 scene.add(mesh1, mesh2, mesh3);
+
+// particles
+// geometry
+const particleCount = 200;
+const positions = new Float32Array(particleCount * 3);
+
+for (let i = 0; i < particleCount; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+    positions[i * 3 + 1] = objectsDistance * 0.4 - Math.random() * objectsDistance * sectionMeshes.length;
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+// material
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.05
+});
+
+// points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 // lights
 const pointlight = new THREE.PointLight("#ffffff", 1);
@@ -69,10 +100,14 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+// camera group for parallax
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
 // camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 6;
-scene.add(camera);
+cameraGroup.add(camera);
 
 // renderer
 const renderer = new THREE.WebGLRenderer({
@@ -85,12 +120,41 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // scroll
 let scrollY = window.scrollY;
 
+window.addEventListener("scroll", () => {
+    scrollY = window.scrollY;
+})
+
+// cursor
+const cursor = {};
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener("mousemove", () => {
+    // get values from -0.5 -> 0.5
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = event.clientY / sizes.height - 0.5;
+})
+
 // animation
 const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
+
+
+    // animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance;
+
+    const parallaxX = cursor.x;
+    const parallaxY = -cursor.y;
+
+    // lerping formula
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) / 1.5 * deltaTime;
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) / 1.5 * deltaTime;
 
     // animate meshes
     for (const mesh of sectionMeshes) {
